86093e493e2aeb58233ca88e77b397e6e28dabff
ArtiZzzzt <artin.shakibi@gmail.com>
2025-07-10 12:00:28 +0200

    Objects and stuff are now in groups

--- main.py
+++ main.py
@@ -5,18 +5,26 @@ from player import *
 
 def main():
     pygame.init()
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     while True:
       for event in pygame.event.get():
          if event.type == pygame.QUIT:
             return
       screen.fill(000)
       dt = clock.tick(60)/1000
-      player.draw(screen)
-      player.update(dt)
+      for objects in drawable:
+         objects.draw(screen)
+      updateable.update(dt)
       
 
       pygame.display.flip()
