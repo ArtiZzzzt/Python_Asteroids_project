d34cf9a2912e2519983c1347717e3ca067cc871c
ArtiZzzzt <artin.shakibi@gmail.com>
2025-07-10 14:10:46 +0200

    asteroidfield

--- main.py
+++ main.py
@@ -1,33 +1,45 @@
 import pygame
 from constants import * 
-from circleshape import *
-from player import *
+from player import Player
+from asteroid import Asteroid
+from AsteroidField import AsteroidField
 
 def main():
     pygame.init()
 
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
     updateable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updateable, drawable)
+    AsteroidField.containers = updateable
     Player.containers = (updateable, drawable)
 
+
+    asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     
-    clock = pygame.time.Clock()
     dt = 0
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
       for event in pygame.event.get():
          if event.type == pygame.QUIT:
             return
+         
+      updateable.update(dt)
+
       screen.fill(000)
-      dt = clock.tick(60)/1000
+      
       for objects in drawable:
          objects.draw(screen)
-      updateable.update(dt)
-      
 
       pygame.display.flip()
+
+      dt = clock.tick(60)/1000
+
 
 if __name__ == "__main__":
     main()
