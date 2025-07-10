1d20878831b56eb053364a109b0e4caf47a6ed4f
ArtiZzzzt <artin.shakibi@gmail.com>
2025-07-10 11:04:19 +0200

    B: Clock

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
       for event in pygame.event.get():
@@ -10,11 +12,8 @@ def main():
             return
       screen.fill(000)  
       pygame.display.flip()
-
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
+      clock.tick(60)
+      dt += clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
