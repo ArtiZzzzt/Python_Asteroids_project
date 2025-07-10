c9569564ea36dec355648a72b5a6a181dd22900e
ArtiZzzzt <artin.shakibi@gmail.com>
2025-07-10 11:33:32 +0200

    D: Player rotation bby

--- main.py
+++ main.py
@@ -14,11 +14,12 @@ def main():
          if event.type == pygame.QUIT:
             return
       screen.fill(000)
+      dt = clock.tick(60)/1000
       player.draw(screen)
+      player.update(dt)
+      
 
       pygame.display.flip()
-      clock.tick(60)
-      dt += clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
