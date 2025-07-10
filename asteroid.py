ec44d719d2626d9cbb58e1d97b64211d9349d24d
ArtiZzzzt <artin.shakibi@gmail.com>
2025-07-10 15:09:47 +0200

    breaking asteroids now splits

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,6 @@
 from circleshape import *
-
+import random
+from constants import ASTEROID_MAX_RADIUS, ASTEROID_MIN_RADIUS
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -11,3 +12,15 @@ class Asteroid(CircleShape):
     def update(self, dt):
         self.position += self.velocity * dt
 
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        random_andgle = random.uniform(20, 50)
+        small1 = self.velocity.rotate(random_andgle)
+        small2 = self.velocity.rotate(-random_andgle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid1 = Asteroid(self.position, self.position, new_radius)
+        asteroid2 = Asteroid(self.position, self.position, new_radius)
+        asteroid1.velocity = small1
+        asteroid2.velocity = small2
